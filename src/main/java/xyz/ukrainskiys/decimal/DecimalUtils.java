5a5c67262f2995d37295113228057ce7c2004467
UkarainskiyS <https://github.com/UkrainskiyS>
2022-11-19 19:43:29 +0300

    delete implementation Decimal from BigDecimal

--- src/main/java/xyz/ukrainskiys/decimal/DecimalUtils.java
+++ src/main/java/xyz/ukrainskiys/decimal/DecimalUtils.java
@@ -5,57 +5,55 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
 
-import static java.math.BigDecimal.ZERO;
-import static java.math.RoundingMode.HALF_UP;
 import static xyz.ukrainskiys.decimal.constant.Decimals.HUNDRED;
+import static xyz.ukrainskiys.decimal.constant.Decimals.ZERO;
 
 public class DecimalUtils {
 
-    public static BigDecimal max(BigDecimal... decimals) {
+    public static Decimal max(Decimal... decimals) {
         return Arrays.stream(decimals)
                 .filter(Objects::nonNull)
-                .max(BigDecimal::compareTo)
+                .max(Decimal::compareTo)
                 .orElse(ZERO);
     }
 
-    public static BigDecimal max(Collection<BigDecimal> decimals) {
-        return max(decimals.toArray(BigDecimal[]::new));
+    public static Decimal max(Collection<Decimal> decimals) {
+        return max(decimals.toArray(Decimal[]::new));
     }
 
 
-    public static BigDecimal min(BigDecimal... decimals) {
+    public static Decimal min(Decimal... decimals) {
         return Arrays.stream(decimals)
                 .filter(Objects::nonNull)
-                .min(BigDecimal::compareTo)
+                .min(Decimal::compareTo)
                 .orElse(ZERO);
     }
 
-    public static BigDecimal min(Collection<BigDecimal> decimals) {
-        return min(decimals.toArray(BigDecimal[]::new));
+    public static Decimal min(Collection<Decimal> decimals) {
+        return min(decimals.toArray(Decimal[]::new));
     }
 
 
-    public static BigDecimal sum(BigDecimal... decimals) {
+    public static Decimal sum(Decimal... decimals) {
         return Arrays.stream(decimals)
                 .filter(Objects::nonNull)
-                .reduce(ZERO, BigDecimal::add);
+                .reduce(ZERO, Decimal::add);
     }
 
-    public static BigDecimal sum(Collection<BigDecimal> decimals) {
-        return sum(decimals.toArray(BigDecimal[]::new));
+    public static Decimal sum(Collection<Decimal> decimals) {
+        return sum(decimals.toArray(Decimal[]::new));
     }
 
-    public static long toKopecks(BigDecimal decimal) {
+    public static long toKopecks(Decimal decimal) {
         return decimal.multiply(HUNDRED).longValue();
     }
 
-    public static BigDecimal fromKopecks(long kopecks) {
-        return new BigDecimal(kopecks)
-                .divide(HUNDRED, 2, HALF_UP);
+    public static Decimal safeGet(Decimal decimal) {
+        return decimal == null ? ZERO : decimal;
     }
 
-    public static BigDecimal safeGet(BigDecimal decimal) {
-        return decimal == null ? ZERO : decimal;
+    public static BigDecimal safeGet(BigDecimal bigDecimal) {
+        return bigDecimal == null ? BigDecimal.ZERO : bigDecimal;
     }
 
 }
